4ba8d731cba2990b148ca49921b88f103ce1a808
Elián Barrios <elianbarrios1707@outlook.com>
2025-07-14 13:40:00 -0600

    Add user creation

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UsersAPI.Data;
+using UsersAPI.Models;
 
 namespace UsersAPI.Namespace
 {
@@ -6,9 +9,79 @@ namespace UsersAPI.Namespace
     [ApiController]
     public class UserController : ControllerBase
     {
-        public IActionResult Get()
+        private UsersAPIDbContext _context;
+
+        public UserController(UsersAPIDbContext context)
         {
-            throw new NotImplementedException("This method is not implemented yet.");
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _context.Users.Select(u => new UserDto
+            {
+                Id = u.Id,
+                FirstName = u.FirstName,
+                LastName = u.LastName,
+                Email = u.Email,
+                Phone = u.Phone,
+                DateOfBirth = u.DateOfBirth,
+                IsActive = u.IsActive
+            }).ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var users = await _context.Users.FindAsync(id);
+
+            if (users == null)
+            {
+                return NotFound();
+            }
+            return Ok(new UserDto
+            {
+                Id = users.Id,
+                FirstName = users.FirstName,
+                LastName = users.LastName,
+                Email = users.Email,
+                Phone = users.Phone,
+                DateOfBirth = users.DateOfBirth,
+                IsActive = users.IsActive
+            });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] UserCreateDto userCreateDto)
+        {
+            if (userCreateDto == null)
+            {
+                return BadRequest("User data is null.");
+            }
+
+            var user = new User
+            {
+                FirstName = userCreateDto.FirstName,
+                LastName = userCreateDto.LastName,
+                Email = userCreateDto.Email,
+                Phone = userCreateDto.Phone,
+                DateOfBirth = userCreateDto.DateOfBirth,
+                IsActive = true
+            };
+
+            await _context.AddAsync(user);
+
+            return Ok(new UserDto
+            {
+                Id = user.Id,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email,
+                Phone = user.Phone,
+                DateOfBirth = user.DateOfBirth,
+                IsActive = user.IsActive
+            });
         }
     }
 }
---
+++ DTOs/UserDto.cs
@@ -0,0 +1,22 @@
+﻿namespace UsersAPI;
+
+public class UserDto
+{
+    public int Id { get; set; }
+
+    public string? FirstName { get; set; }
+
+    public string LastName { get; set; } = null!;
+
+    public string FullName => $"{FirstName} {LastName}".Trim();
+
+    public string Email { get; set; } = null!;
+
+    public string Phone { get; set; } = null!;
+
+    public DateTime DateOfBirth { get; set; }
+    
+     public int Age => DateTime.Now.Year - DateOfBirth.Year;
+
+    public bool IsActive { get; set; }
+}
